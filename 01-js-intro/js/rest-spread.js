784f687ee51e75eda752282d4096847bcc90d2e9
iproduct <office@iproduct.org>
2017-03-02 15:00:23 +0200

    renaming the projects with numbers upfront

--- 01-js-intro/js/rest-spread.js
+++ 01-js-intro/js/rest-spread.js
@@ -25,21 +25,3 @@ c.shift();
 console.log('a=', a);
 console.log('b=', b);
 
-let fibonacci = {
-  [Symbol.iterator]() {
-    let pre = 0, cur = 1;
-    return {
-      next() {
-        [pre, cur] = [cur, pre + cur];
-        return { done: false, value: cur }
-      }
-    }
-  }
-}
-
-for (var n of fibonacci) {
-  // truncate the sequence at 1000
-  if (n > 1000)
-    break;
-  console.log(n);
-}
