f4ed61a5cbbe5cb9f0e89d4822ed81b237f6b65a
iproduct <office@iproduct.org>
2017-03-09 17:45:37 +0200

    webpack 2 React HMR setup complete

---
+++ 09-es6-webpack2/src/app/index.js
@@ -0,0 +1,21 @@
+import './assets/main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import Root from './containers/Root';
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+};
+
+render(Root);
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => { render(Root) });
+}
