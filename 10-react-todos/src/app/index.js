9e427e5948a08c9f6bf341437d89163090d5ef47
iproduct <office@iproduct.org>
2017-03-22 18:04:00 +0200

    todos enter/leave animations added

--- 10-react-todos/src/app/index.js
+++ 10-react-todos/src/app/index.js
@@ -1,4 +1,5 @@
 import './assets/css/main.css';
+import './assets/css/animation.css';
 // import './assets/images/react-redux.png';
 
 import React from 'react';
--- 10-react-todos/src/app/todo-list.js
+++ 10-react-todos/src/app/todo-list.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Todo from './todo';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default ({ todos, filter, ...rest }) => (
   <ul className="todo-list list-group">
-    {todos
-      .filter(todo => filter === 'all' ? true : todo.status === filter)
-      .map(todo => (
-        <li key={todo.id} className="list-group-item">
-          <Todo todo={todo} {...rest} />
-        </li>
-      ))
-    }
+    <ReactCSSTransitionGroup transitionName="todos" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+      {todos
+        .filter(todo => filter === 'all' ? true : todo.status === filter)
+        .map(todo => (
+          <li key={todo.id} className="list-group-item">
+            <Todo todo={todo} {...rest} />
+          </li>
+        ))
+      }
+    </ReactCSSTransitionGroup>
   </ul>
 );
