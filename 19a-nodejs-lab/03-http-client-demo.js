9984868c2d6508671d43079914d9f03840f6bb9b
iproduct <office@iproduct.org>
2017-05-13 16:48:35 +0300

    POST json method added to http server

--- 19a-nodejs-lab/03-http-client-demo.js
+++ 19a-nodejs-lab/03-http-client-demo.js
@@ -1,9 +1,17 @@
 var http = require('http');
 
-const options = { 
-  hostname: 'localhost', 
+const todo = { id: 1, text: 'Add POST handling to server.', status: 'active' };
+const postData = JSON.stringify(todo);
+
+const options = {
+  hostname: 'localhost',
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(postData, 'utf-8'),
+  },
   port: 3000,
-  path: '/page1.html'
+  path: '/api/todos'
 };
 
 const req = http.request(options, (res) => {
@@ -22,4 +30,6 @@ req.on('error', (e) => {
   console.error(`problem with request: ${e.message}`);
 });
 
+// write data to request body
+req.write(postData);
 req.end();
--- 19a-nodejs-lab/05-http-demo-post.js
+++ 19a-nodejs-lab/05-http-demo-post.js
@@ -4,20 +4,22 @@ const querystring = require('querystring');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = 3000;
+const todoDb = [];
+let todoId = 0;
 const server = http.createServer((req, res) => {
-  //parse URL
-  const urlObj = url.parse(req.url);
-  const path = urlObj.path;
-  const queryStr = urlObj.query;
-  const query = querystring.parse(queryStr);
+    //parse URL
+    const urlObj = url.parse(req.url);
+    const path = urlObj.path;
+    const queryStr = urlObj.query;
+    const query = querystring.parse(queryStr);
 
-  console.log(`Path: ${path}`);
-  console.log(`Query: ${JSON.stringify(query)}`);
-  console.log(`Method: ${req.method}`);
-  console.log(`Method: ${JSON.stringify(req.headers)}`);
+    console.log(`Path: ${path}`);
+    console.log(`Query: ${JSON.stringify(query)}`);
+    console.log(`Method: ${req.method}`);
+    console.log(`Method: ${JSON.stringify(req.headers)}`);
 
-  if (req.method === 'GET') {
+    if (req.method === 'GET') {
         // Providing response - read the requested file content from file system
         fs.readFile(path.substr(1), function (err, data) {
             if (err) {
@@ -34,29 +36,38 @@ const server = http.createServer((req, res) => {
             // Send the response body 
             res.end();
         });
-        
-    } 
-    // else if (request.method === 'POST') {
-    //     var body = [];
-    //     request.on('data', function (chunk) {
-    //         body.push(chunk);
-    //     }).on('end', function () {
-    //         body = Buffer.concat(body).toString();
-    //         // at this point, `body` has the entire request body stored in it as a string
-    //         console.log(body);
-           
-    //         // Retun response - 201 : Created
-    //         response.writeHead(201, {
-    //             'content-type': 'text/html',
-    //             'location': `http://localhost:${PORT}/page1.html`
-    //         });
-    //         response.write(`<html><body><a href=http://localhost:${PORT}/page1.html>page1.html</a>`);
-    //         response.end();
-    //     });
-    // }
+
+    }
+    else if (req.method === 'POST') {
+        if (path === '/api/todos') {
+            var body = [];
+            req.on('data', function (chunk) {
+                body.push(chunk);
+            }).on('end', function () {
+                body = Buffer.concat(body).toString();
+                // at this point, `body` has the entire request body stored in it as a string
+                console.log(body);
+                const todo = JSON.parse(body);
+                todo.id = ++todoId;
+                todoDb.push(todo);
+
+                // Retun response - 201 : Created
+                res.writeHead(201, {
+                    'content-type': 'application/json',
+                    'location': `http://localhost:${PORT}/api/todos/${todo.id}`
+                });
+                res.write(JSON.stringify(todo));
+                res.end();
+            });
+        } else {
+            // 404: File not found
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(`<h1>Resource ${path} does not exist`);
+        }
+    }
 });
 
-server.listen(port, hostname, (err) => {
-  if (err) console.error(err);
-  else console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, hostname, (err) => {
+    if (err) console.error(err);
+    else console.log(`Server running at http://${hostname}:${PORT}/`);
 });
