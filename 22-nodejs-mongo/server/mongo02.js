2015bdc6750441be9350433e743b0bf01aded51a
Trayan Iliev <office@iproduct.org>
2019-02-20 21:25:59 +0200

    01-react-todos initial commit

--- 22-nodejs-mongo/server/mongo02.js
+++ 22-nodejs-mongo/server/mongo02.js
@@ -5,7 +5,7 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("articles");
   var author1 = { name: "Eric Evans", address: "New York, USA" };
-  dbo.collection("authors").find({address: /^New/}).project({name: 1}).sort({name: 1}).toArray(function(err, res) {
+  dbo.collection("authors").find({address: /^New/}).project({name: 1, address:1}).sort({name: -1}).toArray(function(err, res) {
     if (err) throw err;
     console.log("1 document found:");
     console.log(res);
