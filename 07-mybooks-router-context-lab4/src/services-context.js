1a78aa74f3cc67a2a6bdab697c7f6e27200d15b8
Trayan Iliev <office@iproduct.org>
2020-04-06 23:32:05 +0300

    advanced react demos added: context, refs, uncontrolled, formik

--- 07-mybooks-router-context-lab4/src/services-context.js
+++
@@ -1,9 +0,0 @@
-import GoogleBooks from "./google-books-service";
-import React from 'react';
-
-export const services = {
-  googleBooksService: new GoogleBooks(),
-}
-
-// Global services context
-export const ServicesContext = React.createContext(services);
