a25ef498961446dd52759c99506438377dcc0d2e
Trayan Iliev <office@iproduct.org>
2020-06-02 00:05:23 +0300

    CRUD for posts completed

--- 12-blogs-api-express-mongo/routes/posts-router.js
+++ 12-blogs-api-express-mongo/routes/posts-router.js
@@ -8,31 +8,40 @@ const util = require('util');
 const router = express.Router();
 
 // Posts API Feature
-router.get('/', (req, res) => {
-    req.app.locals.db.collection('posts').find().toArray().then(posts => {
+router.get('/', async(req, res) => {
+    try{
+        const posts = await req.app.locals.db.collection('posts').find().toArray();
         res.json(posts.map(p => replaceId(p)));
-    });
+    } catch(err) {
+        sendErrorResponse(req, res, 500, `Server error: ${err.message}`, err);
+    }
 });
-router.get('/:id', (req, res) => {
-    const post = req.app.locals.db.collection('posts').findOne(new ObjectID(req.params.id)).then(post => {
+router.get('/:id', async (req, res) => {
+    const params = req.params;
+    try {
+        await indicative.validator.validate(params, { id: 'required|regex:^[0-9a-f]{24}$' });
+        const post = await req.app.locals.db.collection('posts').findOne({ _id: new ObjectID(req.params.id) });
         if (!post) {
-            sendErrorResponse(req, res, 404, `Post with ID=${+req.params.id} does not exist`);
+            sendErrorResponse(req, res, 404, `Post with ID=${req.params.id} does not exist`);
+            return;
         }
         res.json(post);
-    });
+    } catch (errors) {
+        sendErrorResponse(req, res, 400, `Invalid user data: ${util.inspect(errors)}`);
+    }
 });
 
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
     const post = req.body;
     indicative.validator.validate(post, {
         // id: 'required|regex:^[0-9a-f]{24}',
         title: 'required|string|min:2|max:60',
         subtitle: 'string|max:255',
-        description: 'string',
+        content: 'string',
     }).then(() => {
         req.app.locals.db.collection('posts').insertOne(post).then(r => {
             if (r.result.ok && r.insertedCount === 1) {
-                delete  post._id;
+                delete post._id;
                 post.id = r.insertedId;
                 console.log(`Created post: ${post}`);
                 res.status(201).location(`/posts/${post.id}`).json(post);
@@ -48,30 +57,64 @@ router.post('/', function (req, res) {
     });
 });
 
-router.put('/:id', (req, res) => {
-    const old = posts.get(+req.params.id);
+router.put('/:id', async (req, res) => {
+    const old = await req.app.locals.db.collection('posts').findOne({ _id: new ObjectID(req.params.id) });
     if (!old) {
-        sendErrorResponse(req, res, 404, `Post with ID=${+req.params.id} does not exist`);
+        sendErrorResponse(req, res, 404, `Post with ID=${req.params.id} does not exist`);
+        return;
     }
     const post = req.body;
-    if (old.id !== post.id) {
-        sendErrorResponse(req, res, 400, `Post ID=${post.id} does not match URL ID=${+req.params.id}`);
+    if (old._id.toString() !== post.id) {
+        sendErrorResponse(req, res, 400, `Post ID=${post.id} does not match URL ID=${req.params.id}`);
+        return;
     }
-
-    posts.set(post.id, post);
-    console.log(`Updated post: ${post}`);
-    res.json(post);
+    try {
+        await indicative.validator.validate(post, {
+            id: 'required|regex:^[0-9a-f]{24}',
+            title: 'required|string|min:2|max:60',
+            subtitle: 'string|max:255',
+            content: 'string',
+        });
+        try {
+            r = await req.app.locals.db.collection('posts').updateOne({ _id: new ObjectID(req.params.id) }, { $set: post });
+            if (r.result.ok) {
+                delete post._id;
+                console.log(`Updated post: ${JSON.stringify(post)}`);
+                if (r.modifiedCount === 0) {
+                    console.log(`The old and the new posts are the same.`);
+                }
+                res.json(post);
+            } else {
+                sendErrorResponse(req, res, 500, `Unable to update post: ${post.title}`);
+            }
+        } catch (err) {
+            console.log("Error: Update unsuccessfull.");
+            sendErrorResponse(req, res, 500, `Server error: ${err.message}`, err);
+        }
+    } catch (errors) {
+        sendErrorResponse(req, res, 400, `Invalid user data: ${util.inspect(errors)}`);
+    }
 });
 
-router.delete('/:id', (req, res) => {
-    const old = posts.get(+req.params.id);
-    if (!old) {
-        sendErrorResponse(req, res, 404, `Post with ID=${+req.params.id} does not exist`);
+router.delete('/:id', async (req, res) => {
+    const params = req.params;
+    try {
+        await indicative.validator.validate(params, { id: 'required|regex:^[0-9a-f]{24}$' });
+        const old = await req.app.locals.db.collection('posts').findOne({ _id: new ObjectID(req.params.id) });
+        if (!old) {
+            sendErrorResponse(req, res, 404, `Post with ID=${req.params.id} does not exist`);
+            return;
+        }
+        replaceId(old);
+        const r = await req.app.locals.db.collection('posts').deleteOne({ _id: new ObjectID(req.params.id) });
+        if(r.result.ok && r.deletedCount === 1) {
+            res.json(old);
+        } else {
+            sendErrorResponse(req, res, 500, `Unable to delete post: ${old.id}: ${old.title}`);
+        }
+    } catch (errors) {
+        sendErrorResponse(req, res, 400, `Invalid user data: ${util.inspect(errors)}`);
     }
-
-    posts.delete(+req.params.id);
-    console.log(`Deleted post: ${old}`);
-    res.json(old);
 });
 
 module.exports = router;
