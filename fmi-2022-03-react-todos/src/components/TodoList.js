26abbdd91ff05dd222269c797360718189075f13
Trayan Iliev <office@iproduct.org>
2022-03-15 21:22:01 +0200

    TodoItem.css added

--- fmi-2022-03-react-todos/src/components/TodoList.js
+++ fmi-2022-03-react-todos/src/components/TodoList.js
@@ -1,10 +1,11 @@
+import './TodoList.css'
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ALL_STATUSES, ACTIVE, CANCELED, COMPLETED, TodoStatus } from '../model/todo-model'
 
 const TodoList = ({ todos }) => {
     return (
-        <ul>
+        <ul className="TodoList-items">
             {todos.map(todo => (
                 <li key={todo.id}>{todo.id}: {todo.text} - {TodoStatus[todo.status]}</li>
             ))}
