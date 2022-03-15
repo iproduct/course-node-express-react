dc4df7434d688943f418722aae45b03905741c7d
Trayan Iliev <office@iproduct.org>
2022-03-15 21:16:27 +0200

    TodoList component added

--- fmi-2022-03-react-todos/src/components/TodoList.js
+++ fmi-2022-03-react-todos/src/components/TodoList.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ALL_STATUSES, ACTIVE, CANCELED, COMPLETED, TodoStatus } from '../model/todo-model'
 
-const TodoList = props => {
-  return (
-    <div>TodoList</div>
-  )
+const TodoList = ({ todos }) => {
+    return (
+        <ul>
+            {todos.map(todo => (
+                <li key={todo.id}>{todo.id}: {todo.text} - {TodoStatus[todo.status]}</li>
+            ))}
+        </ul>
+    )
 }
 
-TodoList.propTypes = {}
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        status: PropTypes.oneOf([ALL_STATUSES, ACTIVE, COMPLETED, CANCELED])
+    })).isRequired
+}
 
 export default TodoList
