edd809ebb5a09dcd6f25d14c108093db5d229c1a
Trayan Iliev <office@iproduct.org>
2022-03-29 19:08:46 +0300

    status change handling complete

--- fmi-2022-03-react-todos/src/App.js
+++ fmi-2022-03-react-todos/src/App.js
@@ -9,11 +9,14 @@ function App() {
   function deleteTodo(deleted) {
     setTodos(oldTodos => oldTodos.filter(td => td.id !== deleted.id));
   }
+  function statusChanged(changedTodo) {
+    setTodos(oldTodos => oldTodos.map(td => td.id === changedTodo.id? changedTodo : td))
+  }
   return (
     <div className="App-header">
       <h1>React TODOs Demo</h1>
       <TodoInput onSubmitTodo={todo => setTodos(oldTodos => [...oldTodos, todo])} />
-      <TodoList todos={todos} onDelete={deleteTodo} />
+      <TodoList todos={todos} onDelete={deleteTodo} onStatusChange={statusChanged}/>
     </div>
   );
 }
