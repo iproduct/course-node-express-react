1d2ae62952c3cefd83a55ebf73ab64e0b41978f1
Trayan Iliev <office@iproduct.org>
2022-03-29 19:19:05 +0300

    TodoFilter problem template added

--- fmi-2022-03-react-todos/src/App.js
+++ fmi-2022-03-react-todos/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import TodoFilter from './components/TodoFilter';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import MOCK_TODOS from './model/mock-todos';
@@ -12,10 +13,14 @@ function App() {
   function statusChanged(changedTodo) {
     setTodos(oldTodos => oldTodos.map(td => td.id === changedTodo.id? changedTodo : td))
   }
+  function filterChanged(filterText){
+    //You code here
+  }
   return (
     <div className="App-header">
       <h1>React TODOs Demo</h1>
       <TodoInput onSubmitTodo={todo => setTodos(oldTodos => [...oldTodos, todo])} />
+      <TodoFilter onFilterChange={filterChanged} />
       <TodoList todos={todos} onDelete={deleteTodo} onStatusChange={statusChanged}/>
     </div>
   );
---
+++ fmi-2022-03-react-todos/src/components/TodoFilter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TodoFilter = props => {
+  return (
+    <div>TodoFilter: YOUR CODE HERE! (ALL_STATUSES = 0, ACTIVE = 1, COMPLETED = 2, CANCELED = 3) </div>
+  )
+}
+
+TodoFilter.propTypes = {}
+
+export default TodoFilter
