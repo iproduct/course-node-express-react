18d81cbb03366c9f5f69080ce794ea5f4d024283
Trayan Iliev <office@iproduct.org>
2022-04-19 21:31:43 +0300

    tab panel content updated

--- 06-react-blogs/src/component/Main.js
+++ 06-react-blogs/src/component/Main.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PostList, { CARDS } from './PostList';
 import { TabContainer } from './TabContainer';
@@ -18,14 +18,14 @@ class Main extends Component {
             <div className="section">
                 <div className="row">
                     <TabContainer>
-                        <TabPanel id="results" title="All Blogs"> 
-                        <PostList posts={this.props.posts} mode={CARDS} render={
-                            (post, rest) => (<Post key={post.id} post={post} {...rest} />)
-                        }/>
+                        <TabPanel id="results" title="All Blogs">
+                            <PostList posts={this.props.posts} mode={CARDS} render={
+                                (post, rest) => (<Post key={post.id} post={post} {...rest} />)
+                            } />
                         </TabPanel>
                         <TabPanel id="favourites" title="Favourite Blogs">Favs</TabPanel>
-                        <TabPanel id="users" title="Users"><UserForm onSubmit={data=>{console.log(data)}}/></TabPanel>
-                        <TabPanel id="settings" title="Blog Settings">Blog settings here ...</TabPanel>
+                        <TabPanel id="users" title="Users"><UserForm onSubmit={data => { console.log(data) }} /></TabPanel>
+                        <TabPanel id="settings" title="Blog Settings"><h3>Blog settings here ...</h3></TabPanel>
                     </TabContainer>
                 </div>
             </div>
