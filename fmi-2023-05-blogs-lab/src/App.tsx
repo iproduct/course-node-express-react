98b0bcebc0928d13cd8cd2a8979fbdb041c171a8
Trayan Iliev <office@iproduct.org>
2023-04-11 21:40:22 +0300

    conditional component rendering

--- fmi-2023-05-blogs-lab/src/App.tsx
+++ fmi-2023-05-blogs-lab/src/App.tsx
@@ -8,6 +8,7 @@ type Props = {}
 
 const App = (props: Props) => {
     const [posts, setPosts] = useState<Post[]>([]);
+    const [showForm, setShowForm] = useState<boolean>(false);
     useEffect(() => {
         var elems = document.querySelectorAll('.sidenav');
         M.Sidenav.init(elems, {});
@@ -36,45 +37,37 @@ const App = (props: Props) => {
             <div className="section no-pad-bot" id="index-banner">
                 <div className="container">
                     <br /><br />
-                    <h1 className="header center orange-text">Starter Template</h1>
+                    <h1 className="header center orange-text">My Blogs</h1>
                     <div className="row center">
-                        <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                        <h5 className="header col s12 light">React Demo using TypeScript and Redux</h5>
                     </div>
                     <div className="row center">
-                        <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light orange">Get Started</a>
+                        <button id="download-button" className="btn-large range"
+                            onClick={() => setShowForm(show => !show)}>
+                            Add New Post
+                        </button>
                     </div>
                     <br /><br />
                 </div>
             </div>
 
             <div className="container">
-                <div className="section">
+                {showForm && (
+                    <div className="section">
+                        <div className="row">
+                            <h2>Add Post Form</h2>
+                        </div>
+                    </div>
+                )}
 
+                <div className="section">
                     <div className="row">
                         <PostsList posts={posts} />
-
-                        {/* <div className="col s12 m4">
-                            <div className="icon-block">
-                                <h2 className="center light-blue-text"><i className="material-icons">group</i></h2>
-                                <h5 className="center">User Experience Focused</h5>
-
-                                <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-                            </div>
-                        </div>
-
-                        <div className="col s12 m4">
-                            <div className="icon-block">
-                                <h2 className="center light-blue-text"><i className="material-icons">settings</i></h2>
-                                <h5 className="center">Easy to work with</h5>
-
-                                <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-                            </div>
-                        </div> */}
                     </div>
-
                 </div>
                 <br /><br />
             </div>
+
 
             <footer className="page-footer orange">
                 <div className="container">
