e4b497b6e583caaf5a92124b24f03a0cab002f12
Trayan Iliev <office@iproduct.org>
2024-02-19 21:13:38 +0200

    AppTodos added

---
+++ fmi-2024-01-react-todos/src/mock-todos.js
@@ -0,0 +1,14 @@
+import { Todo } from "./todo-model"
+
+const TODOS = [
+    new Todo('Create TODO App using create-react-app', 'completed'),
+    new Todo('Create TodoApp function component'),
+    new Todo('Create TodoList function component'),
+    new Todo('Create TodoItem function component'),
+    new Todo('Create TodoInput function component'),
+    new Todo('Connect todos app to backend with json-server'),
+    new Todo('Create TodoFilter function component'),
+    new Todo('Improve component styling'),
+];
+
+export default TODOS;
