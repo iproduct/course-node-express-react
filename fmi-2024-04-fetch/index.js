f6e36562d63f15f77c83a4508628544da89e5aa5
Trayan Iliev <office@iproduct.org>
2024-03-18 20:39:50 +0200

    promises final solution

--- fmi-2024-04-fetch/index.js
+++ fmi-2024-04-fetch/index.js
@@ -3,19 +3,31 @@ fetch('users.json')
     .then(users => {
         console.log(users);
         const gitUserPromises = users.map(user =>
-            fetch(`https://api.github.com/users/${user.username}`)
+            fetch(`https://github.com/users/${user.username}`)
                 .then(userResp => userResp.json()));
         Promise.allSettled(gitUserPromises)
             .then(gitUserResults => {
                 const gitUsers = gitUserResults.filter(gitUser => gitUser.status === 'fulfilled')
                     .map(gitUser => gitUser.value)
                 console.log(gitUsers)
+                if (gitUsers.length === 0) {
+                    throw 'No users fetched'
+                }
 
-                gitUsers.map(gitUser => {
+                return gitUsers.map(gitUser => {
                     const img = new Image();
                     img.src = gitUser.avatar_url;
                     document.getElementById('results').appendChild(img);
+                    return img;
                 });
             })
-    });
+            .then(images =>new Promise(resolve => setTimeout(resolve, 5000, images))            )
+            .then(images => {
+                images.forEach(img => img.remove());
+            }).catch(err => {
+                console.log(`Error:`, err);
+                document.getElementById('results').innerHTML = `<h2>Error: ${err}</h2>`
+            })
+            .finally(() => console.log('Demo finished.'))
+        });
 
