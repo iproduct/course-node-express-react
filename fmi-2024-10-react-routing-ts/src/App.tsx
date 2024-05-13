42dc57b1a9f587ee8f6fa8af8629642c6d823ade
Trayan Iliev <office@iproduct.org>
2024-05-13 18:58:26 +0300

    initial commit

---
+++ fmi-2024-10-react-routing-ts/src/App.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+function App() {
+  return (<RouterProvider router={router} />);
+}
+
+export default App;
---
+++ fmi-2024-10-react-routing-ts/src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Layout = (props: Props) => {
+    return (
+        <>
+            <div>Hello React Router Demo</div>
+            <nav>
+
+            </nav>
+        </>
+    )
+}
