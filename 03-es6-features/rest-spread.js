b19d87358a3617872f6eec83c617304c1a8aff89
Trayan Iliev <office@iproduct.org>
2026-03-10 18:21:38 +0200

    examples updated

--- 03-es6-features/rest-spread.js
+++ 03-es6-features/rest-spread.js
@@ -4,11 +4,11 @@
 // [a, b] = [b, a];
 // console.log('after swap', a, b); 
 
-// const [a, b, , , ...rest] = [1, 2, 3, 4, 5, 6, 7, 8];
-// console.log(a); // 1
-// console.log(b); // 2
-// console.log(rest); 
-// console.log(...rest); 
+const [a, b, , , ...rest] = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(a); // 1
+console.log(b); // 2
+console.log(rest); 
+console.log(...rest); 
 
 // const obj1 = {a:1, b:2, c:3};
 // const {c, a} = obj1;
@@ -23,33 +23,33 @@
 // console.log(rest);
 // console.log(...(Object.entries(rest)));
 
-const obj = {a:1, b:2, c:3, d:4};
-// const clone = {...obj, c:18, f:42 };
-// console.log(obj);
-// console.log(clone);
-// console.log(clone === obj);
+// const obj = {a:1, b:2, c:3, d:4};
+// // const clone = {...obj, c:18, f:42 };
+// // console.log(obj);
+// // console.log(clone);
+// // console.log(clone === obj);
 
-// const clone2 = Object.assign({}, obj, {c: 18}, {f:42});
-// console.log(obj);
-// console.log(clone2);
-// console.log(clone2 === obj);
+// // const clone2 = Object.assign({}, obj, {c: 18}, {f:42});
+// // console.log(obj);
+// // console.log(clone2);
+// // console.log(clone2 === obj);
 
-const a = [[1], [2], [3]];
-// var b = [ ...a, [4]]; // shallow copy
-// var b = a.concat([[4]]); // shallow copy
-// console.log(b)
+// const a = [[1], [2], [3]];
+// // var b = [ ...a, [4]]; // shallow copy
+// // var b = a.concat([[4]]); // shallow copy
+// // console.log(b)
 
-// const b = JSON.parse(JSON.stringify(a)); // deep copy
-var b = structuredClone(a); // deep copy
-console.log('b=', b, b[0], b[0] === a[0]);
-// console.log('b=', b2, b2[0], b2[0] === a[0]);
-const [, c] = b;
-console.log(c);
-console.log(c.shift());
-console.log(c);
+// // const b = JSON.parse(JSON.stringify(a)); // deep copy
+// var b = structuredClone(a); // deep copy
+// console.log('b=', b, b[0], b[0] === a[0]);
+// // console.log('b=', b2, b2[0], b2[0] === a[0]);
+// const [, c] = b;
+// console.log(c);
+// console.log(c.shift());
+// console.log(c);
 
-// // // // console.log(b.shift().shift());
+// // // // // console.log(b.shift().shift());
 
-console.log('a=', a);
-console.log('b=', b);
+// console.log('a=', a);
+// console.log('b=', b);
 
---
+++ fmi-2026-02-es/let-const.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 10; i++) {
+    setTimeout(function(){
+        console.log(i)
+    }, i * 1000)
+}
