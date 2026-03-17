6d0fbca896375b7bb66ceeaae02cfdedd655d051
Trayan Iliev <office@iproduct.org>
2026-03-17 20:59:43 +0200

    es modules demonstrated

---
+++ fmi-2026-02-es/show-git-users.js
@@ -0,0 +1,23 @@
+export default async (resultsDiv) => {
+    try {
+        const usersResp = await fetch('users.json');
+        const users = await usersResp.json();
+        console.log(users);
+        const usersData = await Promise.allSettled(users.map(async user => {
+            const gitUserResp = await fetch(`https://api.github.com/users/${user.username}`);
+            return gitUserResp.json()
+        }));
+        const gitUserImages = usersData.filter(userData => userData.status === 'fulfilled')
+            .map(userData => userData.value)
+            .map(gitUser => {
+                const img = new Image();
+                img.src = gitUser.avatar_url;
+                resultsDiv.append(img)
+                return img;
+            });
+        await new Promise(resolve => setTimeout(resolve, 10000))
+        gitUserImages.forEach(img => img.remove())
+    } catch(err){
+        console.log(err);
+    }
+}
