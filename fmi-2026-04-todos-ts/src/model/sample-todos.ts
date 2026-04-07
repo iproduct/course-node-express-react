dd3b860c6c353fa1a85fd30f0a331bec6168ceec
Trayan Iliev <office@iproduct.org>
2026-04-07 20:17:33 +0300

    Todo model added

---
+++ fmi-2026-04-todos-ts/src/model/sample-todos.ts
@@ -0,0 +1,12 @@
+import { Todo, TodoStatus } from "./todo";
+
+export const SAMPLE_TODOS = [
+    new Todo('Create TODO App using Vite', TodoStatus.COMPLETED),
+    new Todo('Create App function component'),
+    new Todo('Create TodoList function component'),
+    new Todo('Create TodoItem function component'),
+    new Todo('Create TodoInput stateful function component'),
+    new Todo('Stylize app using Bootstrap 5'),
+    new Todo('Connect Single Page App to json-server backend'),
+    new Todo('Create TodoFilter function component')
+]
---
+++ fmi-2026-04-todos-ts/src/model/todo.ts
@@ -0,0 +1,14 @@
+import type { IdType } from "../shared/common-types";
+
+export class Todo {
+    static nextId = 0;
+    public id: IdType = ++Todo.nextId;
+    constructor(
+        public text: string,
+        public status: TodoStatus = TodoStatus.ACTIVE
+    ) { }
+}
+
+export enum TodoStatus {
+    ACTIVE = 1, COMPLETED, CANCELED
+}
